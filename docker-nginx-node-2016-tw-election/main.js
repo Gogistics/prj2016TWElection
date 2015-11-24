f19a5018056e29c0299246b1dc10a19aa43dcdbc
AlanTai <rainman.tai@gmail.com>
2015-11-24 07:51:12 -0800

    add css block

--- docker-nginx-node-2016-tw-election/main.js
+++ docker-nginx-node-2016-tw-election/main.js
@@ -1,11 +1,11 @@
-// required modules
+/* required modules */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+// end of add modules
 
 // port config.
 var DEFAULT_PORT = 8080;
