0a97021c6ec9992042c8ec3ce6e5f916e40c211d
AlanTai <rainman.tai@gmail.com>
2015-12-08 12:26:54 -0800

    update

--- myWebRTCApp/prjRTCBroadcast/routes/routes.js
+++ myWebRTCApp/prjRTCBroadcast/routes/routes.js
@@ -4,7 +4,7 @@ module.exports = function(app, streams) {
   var index = function(req, res) {
     res.render('index.jade', { title: 'WebRTC', 
                           header: 'WebRTC Broadcast Streaming',
-                          username: 'John Dow',
+                          username: 'Username/Room Tag',
                           share: 'Share this link',
                           footer: 'gogistics@gogistics-tw.com',
                           id: req.params.id
@@ -12,15 +12,15 @@ module.exports = function(app, streams) {
   };
 
   // GET streams as JSON
-  var displayStreams = function(req, res) {
-    var streamList = streams.getStreams();
-    // JSON exploit to clone streamList.public
-    var data = (JSON.parse(JSON.stringify(streamList))); 
+  var display_streams = function(req, res) {
+    var stream_list = streams.getStreams();
+    // JSON exploit to clone stream_list.public
+    var data = (JSON.parse(JSON.stringify(stream_list))); 
 
     res.status(200).json(data);
   };
 
-  app.get('/streams.json', displayStreams);
+  app.get('/streams.json', display_streams);
   app.get('/', index);
   app.get('/:id', index);
 }
