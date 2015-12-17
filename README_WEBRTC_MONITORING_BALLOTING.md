8c441cb7c9284235db2317fc2dc7bdf46420ac2e
AlanTai <rainman.tai@gmail.com>
2015-12-16 21:17:55 -0800

    update stream handler

--- README_WEBRTC_MONITORING_BALLOTING.md
+++ README_WEBRTC_MONITORING_BALLOTING.md
@@ -2,4 +2,5 @@
 
 ### Development of WebRTC Application of Monitoring Balloting (Project is under myWebRTCApp folder)
 
-Description is coming soon and the application will be lanuched ASAP...
+Description is coming soon and the application will be lanuched ASAP.
+First version of the WebRTC App is based the project, https://github.com/pchab/ProjectRTC.
--- myWebRTCApp/prjRTCBroadcast/my_modules/streams.js
+++ myWebRTCApp/prjRTCBroadcast/my_modules/streams.js
@@ -1,4 +1,12 @@
 module.exports = function() {
+  /*
+  * MongoDB Config. (testing)
+  */
+  var monk = require('monk'),
+      url = process.env.MONGODB_USER + ':' + process.env.MONGODB_USER_PWD + '@' + process.env.MONGODB_INSTANCE_DNS + ':27017/test',
+      db = monk(url),
+      streams_collection = db.get('streams_collection');
+
   /**
    * available streams 
    * the id value is considered unique (provided by socket.io)
@@ -6,7 +14,7 @@ module.exports = function() {
   var stream_list = [];
 
   /**
-   * Stream object
+   * Stream object; will be replaced as a document stored in MongoDB
    */
   var Stream = function(id, name, user_type) {
     this.name = name;
@@ -15,11 +23,20 @@ module.exports = function() {
     this.selected_voting_station = ''; // selected_voting_station for regular_user
   }
 
+  /* return streams obj */
   return {
     // add stream
     addStream : function(id, name, user_type) {
       var stream = new Stream(id, name, user_type);
       stream_list.push(stream);
+
+      // new function of adding stream to MongoDB
+      streams_collection.insert(stream, function(err, doc){
+        // successfully store stream doc to MongoDB
+        if(!err){
+          console.log('successfully store stream to MongoDB...');
+        }
+      });
     },
 
     // remove stream
@@ -29,6 +46,14 @@ module.exports = function() {
         index++;
       }
       stream_list.splice(index, 1);
+
+      // new function of removing document (testing)
+      streams_collection.remove({id: id}, function(err){
+        // if error exists, show error
+        if(!err){
+          console.log('successfully remove document from MongoDB...');
+        }
+      });
     },
 
     // update name of stream
@@ -37,10 +62,35 @@ module.exports = function() {
         return element.id == id;
       });
       stream.name = name;
+
+      // new function of updating document (testing)
+      streams_collection.update({id: id}, {$set: {name: name}}, function(err, updated_doc){
+        // if no error, show doc
+        if(!err) console.log(updated_doc);
+      });
     },
 
+    // get streams list
     getStreams : function() {
       return stream_list;
-    }
+    },
+
+    //  new functions for MongoDB (testing)
+    getStreamsList: function(){
+      // update streams list of container based on the data from MongoDB
+      var new_streams_list = [];
+      streams_collection
+      .find({}, {stream: true})
+      .each(function(doc){
+        new_streams_list.push(doc);
+      })
+      .error(function(err){
+        if(err) console.log(err);
+      })
+      .success(function(){
+        //
+        return new_streams_list;
+      });
+    },
   }
 };
